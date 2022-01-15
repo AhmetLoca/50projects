a962462f624c084b847fcd8d5adc3876e303cc13
AhmetLoca <locaahmet@gmail.com>
2022-01-15 12:58:51 +0300

    background-slider

--- project11/script.js
+++ project11/script.js
@@ -5,16 +5,13 @@ const remained= document.getElementById('remained')
 
 updateBigCup()
 
-
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
-
 function highlightCups(idx){
     if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
         idx--
     }
-    
     smallCups.forEach( (cup, idx2) =>{
         if(idx2 <= idx){
             cup.classList.add('full')
@@ -22,11 +19,8 @@ function highlightCups(idx){
         else{
             cup.classList.remove('full')
         }
-
     })
-
     updateBigCup()
-
 }
 
 function updateBigCup(){
@@ -51,5 +45,3 @@ function updateBigCup(){
         listers.innerText = `${2 - (250 * fullCups / 1000)}`
     }
 }
-
-
